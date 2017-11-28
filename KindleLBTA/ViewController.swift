c0144d93d8afc119a88bf344106c9d52dc942d2e
Sieder Villareal <sieder00@gmail.com>
2017-11-28 07:26:05 +0300

    adding uitableview

--- KindleLBTA/ViewController.swift
+++ KindleLBTA/ViewController.swift
@@ -15,10 +15,36 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.tableFooterView = UIView()
+        
+        navigationItem.title = "Kindle"
         
         setupBooks()
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        let book = books?[indexPath.row]
+        
+        cell.textLabel?.text = book?.title
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let count = books?.count {
+            return count
+        }
+        return 0
+    }
+    
+    
     
     func setupBooks() {
         let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: [
@@ -35,13 +61,6 @@ class ViewController: UITableViewController {
         
         self.books = [book, book2]
         
-        guard let books = self.books else { return }
-        for book in books {
-            print(book.title)
-            for page in book.pages {
-                print(page.text)
-            }
-        }
     }
 }
 
